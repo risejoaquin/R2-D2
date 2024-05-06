78cbf81ac40ab99c95a45517441a8903b83949bb
lucilfer <144937463+theprostipirugolf0@users.noreply.github.com>
2024-05-06 14:26:56 -0800

    Add files via upload

---
+++ R2-D2/src/main/java/mx/itson/r2d2/entities/R2D2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.r2d2.entities;
+
+/**
+ *
+ * @author compu
+ */
+public class R2D2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
