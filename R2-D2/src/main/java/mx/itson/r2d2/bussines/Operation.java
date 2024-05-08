429a108747d6fa5ff3ce1e404725ca83518be254
Julian716 <alex.313.rodriguez@gmail.com>
2024-05-08 12:27:10 -0700

    Update Operation.java
    Ya modifique mi parte, favor de comprobar

--- R2-D2/src/main/java/mx/itson/r2d2/bussines/Operation.java
+++ R2-D2/src/main/java/mx/itson/r2d2/bussines/Operation.java
@@ -11,5 +11,6 @@ package mx.itson.r2d2.bussines;
 public class Operation {
     public static void main(String[] args) {
         System.out.println(":)");
+        System.out.println("Salchichon");
     }
 }
