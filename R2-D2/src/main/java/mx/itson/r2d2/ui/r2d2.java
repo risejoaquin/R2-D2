4006ca4147bddf51ae97858ba4c2f4396b4e380e
alumnog <alumnog@S923_21>
2024-05-08 12:14:36 -0700

    Cambio del print

---
+++ R2-D2/src/main/java/mx/itson/r2d2/ui/r2d2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package mx.itson.r2d2.ui;
+
+/**
+ *
+ * @author alumnog
+ */
+public class r2d2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ System.out.println("y puro natanel cano viejones")   
+}
+
+}
